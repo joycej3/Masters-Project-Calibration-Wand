180b92e497493b6c422b151c8a4a236c152fd3a6
joycej3 <joycej3@tcd.ie>
2023-04-26 15:44:06 +0100

    updates

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
+import itertools
 from matplotlib import pyplot as plt
 import numpy as np
 import cv2
 
 from sort_leds import sort_leds
-from calculate_angle import calulate_angle
+# from calculate_angle import calculate_angle
 from process_img import find_connections
 from rotate import rotate_image
 
+
+VideoPath_1 = r"D:\John\Project\Trial1\Cal 02.2144931.20220906113403.avi"
+# VideoPath_1 = r"D:\John\Project\GOPRO\Cal02214490420220906113403.avi"
+template = cv2.imread('D:\John\Project\Code\masters-project\wtemplate.png') 
+
+#       0   1   2
+#           3      
+#           4
+#          
+# Geometric constraints
+mm_wand_length = {
+    (0, 1): 160,
+    (0, 2): 240,
+    (0, 3): 203,
+    (0, 4): 293,
+    (1, 2): 80,
+    (1, 3): 125,
+    (1, 4): 245,
+    (2, 3): 148,
+    (2, 4): 258,
+    (3, 4): 120
+}
+
+# Temporal consistency
+persistent_leds = []
 def click_event(event, x, y, flags, params):
   
     # checking for left mouse clicks
@@ -43,30 +69,121 @@ def click_event(event, x, y, flags, params):
                     (255, 255, 0), 2)
         cv2.imshow('image', img)
   
-VideoPath_1 = r"D:\John\Project\Trial1\Cal 02.2144931.20220906113403.avi"
-template = cv2.imread('D:\John\Project\Code\masters-project\wtemplate.png') 
+def preprocess_image(img):
+    fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
+    fgmask = fgbg.apply(img)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1))
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel)
+    # blurred = cv2.GaussianBlur(img, (5, 5), 0)  # add Gaussian blur
 
-#       0   1   2
-#           3      
-#           4
-#          
-mm_wand_lenth_0_to_1 = 160
-mm_wand_lenth_0_to_2 = 240
-mm_wand_lenth_0_to_3 = 203
-mm_wand_lenth_0_to_4 = 293
-mm_wand_lenth_1_to_2 = 80
-mm_wand_lenth_1_to_3 = 125
-mm_wand_lenth_1_to_4 = 245
-mm_wand_lenth_2_to_3 = 148
-mm_wand_lenth_2_to_4 = 258
-mm_wand_lenth_3_to_4 = 120
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    l, a, b = cv2.split(img)
+    l = clahe.apply(l)
+    img = cv2.merge((l, a, b))
+    img = cv2.cvtColor(img, cv2.COLOR_LAB2BGR)
+ 
+    # blurred = cv2.medianBlur(img, 3) 
+    edges = cv2.Canny(img, 100, 200)
 
-# real_points1 = np.float32([[0, 0,0 ], [160, 0,0], [240, 0,0], [160, 80,0], [160, 245,0]])
-# real_points = []
-# real_points.append(real_points1)
+    return edges
 
-#.......................................................
-#Stats
+
+
+def detect_leds(img, fgmask):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    lower_red1 = np.array([0, 240, 120])
+    upper_red1 = np.array([5, 255, 255])
+
+    lower_red2 = np.array([175, 240, 120])
+    upper_red2 = np.array([180, 255, 255])
+
+    lower_white = np.array([0, 0, 240])
+    upper_white = np.array([255, 200, 255])
+
+
+    mask_red1 = cv2.inRange(hsv, lower_red1, upper_red1)
+    mask_red2 = cv2.inRange(hsv, lower_red2, upper_red2)
+    mask_white = cv2.inRange(hsv, lower_white, upper_white)
+
+    mask = cv2.bitwise_or(mask_red1, mask_red2)
+    mask = cv2.bitwise_or(mask, mask_white)
+    mask = cv2.bitwise_and(mask, fgmask)
+
+    kernel = np.ones((3, 3), np.uint8)
+    mask = cv2.dilate(mask, kernel, iterations=1)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+    
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    led_contours = []
+    excluded_contours = []
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        perimeter = cv2.arcLength(contour, True)
+        circularity = 4 * np.pi * area / perimeter**2 if perimeter != 0 else 0
+
+        if  50 < area < 500 and circularity > 0.5 :#and circularity < 0.25:
+            led_contours.append(contour)
+        else:
+            excluded_contours.append(contour)
+
+    return led_contours, excluded_contours
+
+
+def filter_leds_by_geometric_constraints(leds):
+    filtered_leds = []
+    for (i1, led1), (i2, led2) in itertools.combinations(enumerate(leds), 2):
+        M1 = cv2.moments(led1)
+        M2 = cv2.moments(led2)
+        cX1 = int(M1["m10"] / M1["m00"])
+        cY1 = int(M1["m01"] / M1["m00"])
+        cX2 = int(M2["m10"] / M2["m00"])
+        cY2 = int(M2["m01"] / M2["m00"])
+
+        distance = np.sqrt((cX2 - cX1)**2 + (cY2 - cY1)**2)
+        # print(distance)
+
+        pair_index = (i1, i2)
+        # print(pair_index)
+
+        if pair_index in mm_wand_length:
+            # if mm_wand_length[pair_index] * 0.2 >= distance :#<= mm_wand_length[pair_index] * 1.75:
+             if  distance >= mm_wand_length[pair_index] * 1.75:
+                filtered_leds.extend([led1, led2])
+    return filtered_leds
+
+def update_persistent_leds(leds, persistent_leds, persistence_threshold=2):
+    current_leds = [cv2.boundingRect(led) for led in leds]
+    updated_persistent_leds = []
+
+    for led in current_leds:
+        found_match = False
+        for i, (p, count) in enumerate(persistent_leds):
+            if overlap(led, p):
+                updated_persistent_leds.append((led, count + 1))
+                found_match = True
+                break
+        if not found_match:
+            updated_persistent_leds.append((led, 1))
+
+    persistent_leds = [p for p in updated_persistent_leds if p[1] >= persistence_threshold]
+    return persistent_leds
+
+def overlap(rect1, rect2):
+    # print("rect1: ", rect1)
+    # print("rect2: ", rect2)
+    x1, y1, w1, h1 = rect1
+    x2, y2, w2, h2 = rect2
+    return (x1 < x2 + w2) and (x2 < x1 + w1) and (y1 < y2 + h2) and (y2 < y1 + h1)
+
+
+# def main():
+# print("tests")
 cap = cv2.VideoCapture(VideoPath_1)
 frames_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 fps= int(cap.get(cv2.CAP_PROP_FPS))
@@ -74,208 +191,51 @@ time = frames_count /fps
 frame_progress = 0
 #.......................................................
 
-
 fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
 #Check if file is opened
 if (cap.isOpened()== False):
     print("Error opening video")
-
-while(cap.isOpened()):
+# print("0")
+while (cap.isOpened()):
     ret, img = cap.read()
-    
     if ret == True:
-        
-        
-        frame_progress = frame_progress + 1
-        # if frame_progress > 1102:
-        if frame_progress > 400:
-
-            img_height, img_width = img.shape[:2]
-            led_pixels = np.zeros((img_height, img_width, 3), dtype=np.uint8)
-            og=img.copy()
-
+        frame_progress += 1
+        if frame_progress > 500:
             fgmask = fgbg.apply(img)
-
-            # Apply morphology operations to clean up the mask
             kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
             fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
             fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel)
-
-            # Apply color thresholding to detect saturated LEDs within the moving region
-            hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-            lower = np.array([0, 0, 220 ])
-            upper = np.array([255, 255, 255])
-
-            # Create a mask of saturated pixels using the color range
-            mask = cv2.inRange(hsv, lower, upper)
-            mask = cv2.bitwise_and(mask, fgmask)
-
-            # Apply the mask to the original image to extract the saturated pixels
-            saturated_pixels = cv2.bitwise_and(img, img, mask=mask)
-
-            # Apply color thresholding to detect red LEDs within the moving region
-            lower_red = np.array([0, 100, 100])
-            upper_red = np.array([20, 255, 255])    
-            red_mask = cv2.inRange(hsv, lower_red, upper_red)
-            kernel = np.ones((3,3),np.uint8)
-            red_mask = cv2.dilate(red_mask, kernel, iterations=1)
-
-            mask = cv2.bitwise_and(fgmask, mask)
-            mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-            mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)        
-
-            # 
-
-            
-            contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            led_contours = []
-            for contour in contours:
-                area = cv2.contourArea(contour)
-                perimeter = cv2.arcLength(contour, True)
-                if perimeter != 0:
-                    circularity = 4 * np.pi * area / perimeter**2
-                else:
-                    circularity = 0 
-                if area > 10 and area < 100 and circularity > 0.25:
-                    # print(contour)
-                    led_contours.append(contour)
-
-            # Draw the contours on the original image
-            cv2.drawContours(img, led_contours, -1, (255, 0, 0), -1)
-            cv2.imshow("cont", img)
-            cv2.imshow("original", og)
-            cv2.setMouseCallback('cont', click_event)
-     
-            # print(len(led_contours))
-            #process found led frame
-            if len(led_contours) !=6546:
-                cv2.drawContours(led_pixels, led_contours, -1, (255, 0, 0), -1)
-                gray_mask = cv2.cvtColor(led_pixels, cv2.COLOR_BGR2GRAY)
-                gray_img = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-                surf = cv2.xfeatures2d.SURF_create()
-                kp_mask, des_mask = surf.detectAndCompute(gray_mask, None)
-                kp_img, des_img = surf.detectAndCompute(gray_img, None)
-                bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=False)
-                matches = bf.match(des_mask, des_img)
-                matches = [m for m in matches if m.distance < 100]
-
-                # Get keypoints for the filtered matches
-                pts_mask = np.float32([kp_mask[m.queryIdx].pt for m in matches])
-                pts_img = np.float32([kp_img[m.trainIdx].pt for m in matches])
-
-                # Use the remaining matches to estimate the homography matrix between the two images
-                H, _ = cv2.findHomography(pts_mask, pts_img, cv2.RANSAC)
-
-                # Warp the original image using the homography matrix to obtain an aligned image of the calibration wand
-                aligned_img = cv2.warpPerspective(template, H, (template.shape[1], template.shape[0]))
-
-                # Display the aligned image
-                cv2.imshow('Aligned Image', aligned_img)
-                cv2.waitKey(0)
-
-
-                 # ............................................................................................
-                # try:
-                # cx, cy = find_connections (img, led_contours)  
-                # cy = [None] * 5
-                # cx = [None] * 5
-                # cx[0] = 495
-                # cx[1] = 436
-                # cx[2] = 412
-                # cx[3] = 461
-                # cx[4] = 482
-
-                # cx[0] = 474
-                # cx[1] = 443
-                # cx[2] = 426
-                # cx[3] = 396
-                # cx[4] = 353
-                # # img_points = np.array([[495, 474], [436, 443], [412, 426], [461, 396], [482, 353]], dtype=np.float32)
-                # # real_points1 = np.float32([[0, 0,0 ], [160, 0,0], [240, 0,0], [160, 80,0], [160, 245,0]])
-                # img_points = []
-                # obj_points = []
-                # camera_matrix = np.zeros((3, 3))
-                # dist_coeffs = np.zeros((5, 1))
-
-                # image_points = np.array([[495, 474], [436, 443], [412, 426], [461, 396], [482, 353]], dtype=np.float32)
-                # img_points.append(image_points)  
-                # # The focal length of the camera is 307.8219886231129 pixels
-                # # The focal length of the camera is 547.239090885534 mm
-            
-                # image_points = np.array([[235, 436], [195, 367], [175, 335], [251, 332], [311, 291]], dtype=np.float32)
-                # img_points.append(image_points)  
-                # # The focal length of the camera is 115.55118389395169 pixels
-                # # The focal length of the camera is 205.42432692258078 mm
-
-                # image_points = np.array([[1021, 127], [1007, 183], [1001, 215], [964, 188], [917, 196]], dtype=np.float32)
-                # img_points.append(image_points)  
-
-                # image_points = np.array([[1000, 336], [962, 3773], [936, 402], [920, 354], [877, 335]], dtype=np.float32)
-                # img_points.append(image_points)  
- 
-
-
-                # object_points = np.array([[0, 0,0 ], [160, 0,0], [240, 0,0], [160, 80,0], [160, 245,0]], dtype=np.float32)
-                # obj_points.append(object_points)
-                # obj_points.append(object_points)
-                # obj_points.append(object_points)
-                # obj_points.append(object_points)
-                # retval, cameraMatrix, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, (img_height, img_width), None, None)      
+            img_height, img_width = img.shape[:2]
             
 
-                # # undistorted_img = cv2.undistort(img, camera_matrix, dist_coeffs)
-                # focal_length_px = cameraMatrix[0, 0]
-                # focal_length_mm = focal_length_px * (img_width/img_height)
-                # # print(f"The focal length of the camera is {focal_length_px} pixels")
-                # # print(f"The focal length of the camera is {focal_length_mm} mm")
-                
-                # h,  w = img.shape[:2]
-                # newcameramtx, roi = cv2.getOptimalNewCameraMatrix(cameraMatrix, dist, (w,h), 1, (w,h))
-                # dst = cv2.undistort(img, cameraMatrix, dist, None, newcameramtx)
-                # # crop the image
-                # x, y, w, h = roi
-                # dst = dst[y:y+h, x:x+w]
-                # cv2.imwrite('calibresult.png', dst)
-                # ............................................................................................
+            detected_leds, excludes_leds = detect_leds(img, fgmask)
+            cv2.drawContours(img, detected_leds, -1, (255, 0, 0), -1)
+            cv2.drawContours(img, excludes_leds, -1, (0, 0, 255), -1)
+            cv2.imshow('image', img)
+            cv2.setMouseCallback('image', click_event)
 
-                # if cv2.waitKey(1) & 0xFF == ord('q'):
-                #     break
-                
-                # print("found connection")
-                # cx, cy = sort_leds(cx, cy)
-                # print("sorted")
-                # calulate_angle(cx, cy)
-                # except Exception:
-                # print("exception thrown")
-                # pass
-                # else: 
-                # cv2.line(img, (cx[0], cy[0]), (cx[1], cy[1]), (0, 255, 0), 4 )
-                # # cv2.line(img, (cx[0], cy[0]), (cx[2], cy[2]), (0, 255, 0), 2 )
-                # cv2.line(img, (cx[0], cy[0]), (cx[3], cy[3]), (0, 255, 0), 4 )
-                # cv2.line(img, (cx[0], cy[0]), (cx[4], cy[4]), (0, 255, 0), 4 )
-                # cv2.line(img, (cx[1], cy[1]), (cx[2], cy[2]), (0, 255, 0), 4 )
-                # # cv2.line(img, (cx[1], cy[1]), (cx[4], cy[4]), (0, 255, 0), 2 )
-                # cv2.line(img, (cx[2], cy[2]), (cx[4], cy[4]), (0, 255, 0), 4 )
-                # cv2.line(img, (cx[2], cy[2]), (cx[3], cy[3]), (255, 0, 0), 4 )
-                # cv2.line(img, (cx[1], cy[1]), (cx[3], cy[3]), (0, 0, 255), 2 )
-                # cv2.line(img, (cx[3], cy[3]), (cx[4], cy[4]), (0, 0, 255), 2 )
+            blank_filter= np.zeros((img_height, img_width, 3), dtype=np.uint8)
+            filtered_leds = filter_leds_by_geometric_constraints(detected_leds)
+            cv2.drawContours(blank_filter,  filtered_leds, -1, (255, 255, 255), -1)
+            # cv2.imshow('filter', blank_filter)
 
-            
-        
-        # # Press Q on keyboard to exit
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+            blank_persistent= np.zeros((img_height, img_width, 3), dtype=np.uint8)
+            persistent_leds = update_persistent_leds(detected_leds, persistent_leds)        
+            cv2.drawContours(blank_persistent, persistent_leds, -1, (255, 0, 0), -1)
+            cv2.imshow('persisntent', blank_persistent)
+            # match_leds(template,img )
+
+
+
+        if cv2.waitKey(1) and 0xFF == ord('q'):
             break
+
 
     else:
         print("Fin")
         break
-
-    
-
-# When everything done, release
-# the video capture object
 cap.release()
- 
+
 # Closes all the frames
 cv2.destroyAllWindows()
 
